8f6595363c40ffe0eb1b8369a71673a19be6e17e
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 05:57:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,7 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager"
   }
   return "You are not a teenager"
 
